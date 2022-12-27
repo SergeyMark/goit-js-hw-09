1e448dc134d6eecaaca7d4f5c136834c38a02f00
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-27 22:26:24 +0200

    add up

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,18 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+//   Если пользователь выбрал дату в прошлом, покажи window.alert() с текстом 
+//   "Please choose a date in the future".
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    //   Если пользователь выбрал дату в прошлом, покажи window.alert() с текстом "Please choose a date in the future".
-      if (selectedDates) {
+      const currentDate = new Date();
+      
+      if (selectedDates[0] - currentDate < 0) {
         alert('Please choose a date in the future');
       }
     },
