5d8ad61a8465a2f6440a7692d4fb3af1c7b17bd5
Sergey <110375004+SergeyMark@users.noreply.github.com>
2022-12-27 14:08:58 +0200

    add up

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const bodyTag = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop');
 
+disebledStop();
+
+let intervalId = null;
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    intervalId = setInterval(()=>{
+        bodyTag.style.backgroundColor = getRandomHexColor();
+    }, 1000);    
+
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    
+};
+
+function onBtnStop() {
+    clearInterval(intervalId);
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function disebledStop() {
+    btnStop.disabled = true;
+};
